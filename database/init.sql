264bfd5e5c45055bb31e959b88081084ce0e277b
kvamvasa <cathringo@gmail.com>
2025-11-04 00:47:59 +0200

    additions to schema

--- database/init.sql
+++ database/init.sql
@@ -7,24 +7,44 @@ CREATE COLLATION IF NOT EXISTS case_insensitive_ai (
   deterministic = false
 );
 
-CREATE COLLATION IF NOT EXISTS case_insensitive_ai (
-  provider = icu,
-  locale = 'und-u-ks-level1',
-  deterministic = false
+
+-- Schema migrations table
+CREATE TABLE IF NOT EXISTS schema_migrations (
+    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    version TEXT NOT NULL UNIQUE,
+    applied_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+--TODO check what actually needs to be in master index
 -- Master index table
 CREATE TABLE master_index (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    content_type TEXT NOT NULL,
+    content_type TEXT NOT NULL CHECK (content_type IN ('user', 'post', 'comment', 'group', 'message', 'event')),
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_master_type ON master_index(content_type);
 
+
+-- Auth users table
+CREATE TABLE auth_user (
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    identifier CITEXT COLLATE case_insensitive_ai UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    failed_attempts INTEGER NOT NULL DEFAULT 0,
+    last_login_at TIMESTAMPTZ,
+    CONSTRAINT ux_auth_user_user UNIQUE (user_id),
+    CONSTRAINT ux_auth_user_identifier UNIQUE (identifier),
+    CHECK (identifier = btrim(identifier))
+);
+
+
 -- Users table
 CREATE TABLE users (
     id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    public_id UUID NOT NULL DEFAULT gen_random_uuid(),  
     username CITEXT COLLATE case_insensitive_ai UNIQUE,
     email CITEXT COLLATE case_insensitive_ai UNIQUE,
     first_name VARCHAR(255) NOT NULL,
@@ -42,84 +62,63 @@ CREATE TABLE users (
     )
 );
 
---TODO how to store followers and following
+    CREATE UNIQUE INDEX idx_users_public_id ON users(public_id);
 
--- Auth users table
-CREATE TABLE auth_user (
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    identifier CITEXT COLLATE case_insensitive_ai UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ,
-    failed_attempts INTEGER NOT NULL DEFAULT 0,
-    last_login_at TIMESTAMPTZ,
-    CONSTRAINT ux_auth_user_user UNIQUE (user_id),
-    CONSTRAINT ux_auth_user_identifier UNIQUE (Identifier),
-    CHECK (Identifier = btrim(Identifier))
+--follows table
+CREATE TABLE follows (
+    follower_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    following_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (follower_id, following_id)
 );
 
-
-
--- Conversations table 
---TODO check if better as two separate tables
-CREATE TABLE conversations (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    dm BOOLEAN NOT NULL,
-    group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-    CONSTRAINT dm_group_constraint
-        CHECK (
-            (dm = TRUE AND group_id IS NULL) OR
-            (dm = FALSE AND group_id IS NOT NULL)
-        )
+--follow requests table
+CREATE TABLE follow_requests (
+    requester_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    target_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'rejected')),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (requester_id, target_id)
 );
 
--- Groups table 
+-- Groups table -- TODO keep owner here or in members?
 CREATE TABLE groups (
     id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    group_admin BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
+    group_owner BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
     group_title TEXT NOT NULL,
     group_description TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 
 );
 
--- Messages table
-CREATE TABLE messages (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    conversation_id BIGINT NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
-    sender BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
-    message_text TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    delivered BOOLEAN NOT NULL DEFAULT TRUE,
-    edited_at TIMESTAMPTZ
-);
-
--- Conversation members table
-CREATE TABLE conversation_member (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    last_read_message_id BIGINT REFERENCES messages(id) ON DELETE SET NULL,
-    conversation_id BIGINT NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
-    CONSTRAINT conversation_member_unique UNIQUE (conversation_id, user_id)
-);
-CREATE INDEX idx_conversation_member_conversation ON conversation_member(conversation_id);
-CREATE INDEX idx_conversation_member_user ON conversation_member(user_id);
-
 -- Group members table
--- TODO where to store pending membership (invite received, request sent)
-CREATE TABLE group_member (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT group_member_unique UNIQUE (group_id, user_id)
+CREATE TABLE group_members (
+    group_id BIGINT REFERENCES groups(id) ON DELETE CASCADE,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    role TEXT DEFAULT 'member' CHECK (role IN ('member', 'admin', 'owner')),
+    joined_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (group_id, user_id)
 );
 
--- Schema migrations table
-CREATE TABLE IF NOT EXISTS schema_migrations (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    version TEXT NOT NULL UNIQUE,
-    applied_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- group join requests table
+CREATE TABLE group_join_requests (
+    group_id BIGINT REFERENCES groups(id) ON DELETE CASCADE,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'approved', 'rejected')),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (group_id, user_id)
 );
+
+-- group invites table
+CREATE TABLE group_invites (
+    group_id BIGINT REFERENCES groups(id) ON DELETE CASCADE,
+    sender_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    receiver_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'accepted', 'declined', 'expired')),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (group_id, receiver_id)
+);
+
 
 CREATE TYPE post_visibility AS ENUM ('public', 'almost_private', 'private');
 
@@ -131,19 +130,7 @@ CREATE TABLE posts (
     post_creator BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
     group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
     visibility post_visibility NOT NULL DEFAULT 'public',
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- Events table
--- TODO where to store going/not going
-CREATE TABLE events (
-    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
-    event_title TEXT NOT NULL,
-    event_body TEXT NOT NULL,
-    event_creator BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
-    group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
-    event_date DATE NOT NULL,
-    still_valid BOOLEAN NOT NULL DEFAULT TRUE,
+    image_id BIGINT REFERENCES images(id) ON DELETE SET NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -154,9 +141,79 @@ CREATE TABLE comments (
     parent_id BIGINT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     comment_body TEXT NOT NULL,
     group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
+    image_id BIGINT REFERENCES images(id) ON DELETE SET NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+
+-- Events table
+CREATE TABLE events (
+    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    event_title TEXT NOT NULL,
+    event_body TEXT NOT NULL,
+    event_creator BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
+    group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
+    event_date DATE NOT NULL,
+    still_valid BOOLEAN NOT NULL DEFAULT TRUE,
+    image_id BIGINT REFERENCES images(id) ON DELETE SET NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Event response table
+CREATE TABLE event_response (
+     id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+     event_id BIGINT REFERENCES events(id) ON DELETE CASCADE,
+     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+     going BOOLEAN,
+     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     CONSTRAINT ux_event_user UNIQUE (event_id, user_id)
+);
+
+-- Images table
+CREATE TABLE images (
+     id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+     file_name TEXT,
+     entity_id BIGINT NOT NULL REFERENCES master_index(id) ON DELETE CASCADE,
+     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Conversations table 
+--TODO check if better as two separate tables
+CREATE TABLE conversations (
+    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    dm BOOLEAN NOT NULL,
+    group_id BIGINT REFERENCES groups(id) ON DELETE SET NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT dm_group_constraint
+        CHECK (
+            (dm = TRUE AND group_id IS NULL) OR
+            (dm = FALSE AND group_id IS NOT NULL)
+        )
+);
+
+-- Conversation members table
+CREATE TABLE conversation_members (
+    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    last_read_message_id BIGINT REFERENCES messages(id) ON DELETE SET NULL,
+    conversation_id BIGINT NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
+    CONSTRAINT conversation_member_unique UNIQUE (conversation_id, user_id)
+);
+CREATE INDEX idx_conversation_member_conversation ON conversation_member(conversation_id);
+CREATE INDEX idx_conversation_member_user ON conversation_member(user_id);
+
+
+-- Messages table
+CREATE TABLE messages (
+    id BIGINT PRIMARY KEY REFERENCES master_index(id) ON DELETE CASCADE,
+    conversation_id BIGINT NOT NULL REFERENCES conversations(id) ON DELETE CASCADE,
+    sender BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
+    message_text TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    delivered BOOLEAN NOT NULL DEFAULT TRUE,
+    edited_at TIMESTAMPTZ
+);
 
 
 -- Reactions table
@@ -175,3 +232,6 @@ CREATE TABLE reaction_details (
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE NO ACTION,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
