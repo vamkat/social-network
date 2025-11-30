2ede402f31f90e5f2c96e88924058d769f9a53b6
agioldas <aleksis.gioldaseas@outlook.com>
2025-11-30 13:03:19 +0200

    added register and login servers handlers to userservice server

--- backend/services/users/internal/server/handlers.go
+++ backend/services/users/internal/server/handlers.go
@@ -40,3 +40,46 @@ func (s *Server) GetUserProfile(ctx context.Context, req *pb.GetUserProfileReque
 		Username: profile.Username,
 	}, nil
 }
+
+func (s *Server) RegisterUser(ctx context.Context, req *pb.RegisterUserRequest) (*pb.User, error) {
+	fmt.Println("RegisterUser gRPC method called")
+	user, err := s.Service.RegisterUser(ctx, application.RegisterUserRequest{
+		Username:    req.GetUsername(),
+		FirstName:   req.GetFirstName(),
+		LastName:    req.GetLastName(),
+		Email:       req.GetEmail(),
+		Password:    req.GetPassword(),
+		DateOfBirth: req.GetDateOfBirth().AsTime(),
+		Avatar:      req.GetAvatar(),
+		About:       req.GetAbout(),
+		Public:      req.GetPublic(),
+	})
+	if err != nil {
+		fmt.Println("Error in RegisterUser:", err)
+		return nil, err
+	}
+
+	return &pb.User{
+		UserId:   user.UserId,
+		Username: user.Username,
+		Avatar:   user.Avatar,
+	}, nil
+}
+
+func (s *Server) LoginUser(ctx context.Context, req *pb.LoginRequest) (*pb.User, error) {
+	fmt.Println("LoginUser gRPC method called")
+	user, err := s.Service.LoginUser(ctx, application.LoginRequest{
+		Identifier: req.GetIdentifier(),
+		Password:   req.GetPassword(),
+	})
+	if err != nil {
+		fmt.Println("Error in LoginUser:", err)
+		return nil, err
+	}
+
+	return &pb.User{
+		UserId:   user.UserId,
+		Username: user.Username,
+		Avatar:   user.Avatar,
+	}, nil
+}
