85cdb3fbde74b31c45f3aa570d7f44bc497b2a45
ychaniot <ypatioschaniotakos@gmail.com>
2025-12-14 22:26:34 +0200

    new handler testing

--- social-sphere/src/app/(main)/feed/friends/page.js
+++ social-sphere/src/app/(main)/feed/friends/page.js
@@ -1,14 +1,32 @@
-import {LogoutButton} from "@/components/LogoutButton";
+import { LogoutButton } from "@/components/LogoutButton";
+// import { createGroup } from "@/services/groups/createGroup";
+// import { getAllGroups } from "@/services/groups/getAllGroups";
+// import { getUserGroups } from "@/services/groups/getUserGroups";
+
 
 export const metadata = {
     title: "Friends Feed",
 }
 
-export default function PublicFeedPage() {
-    
+// export async function createaGroup() {
+//     const resp = await createGroup({
+//         group_title: "Friendsdv sgfsdvbsdbbsgfbsfv sbv d ",
+//         group_description: "Frierbgsdfdsbfgbends fv sfv gsfv sfbroup",
+//         group_image: "hello",
+//     });
+//     return resp;
+// }
+
+export default async function FriendsFeedPage() {
+    // const resp = await createaGroup();
+    // console.log(resp);
+
+    // const resp = await getUserGroups({ limit: 10, offset: 0 });
+    // console.log(resp);
+
     return (
         <div>
-            <LogoutButton/>
+            <LogoutButton />
         </div>
     );
 }
--- social-sphere/src/app/(main)/layout.js
+++ social-sphere/src/app/(main)/layout.js
@@ -1,5 +1,14 @@
+import Navbar from "@/components/layout/Navbar";
+
 export const dynamic = 'force-dynamic';
 
 export default function MainLayout({ children }) {
-    return <>{children}</>;
+    return (
+        <div className="min-h-screen flex flex-col bg-(--muted)/6">
+            <Navbar />
+            <main className="flex-1 w-full">
+                {children}
+            </main>
+        </div>
+    );
 }
