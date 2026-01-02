89835c66101a551a4323af5dcd21b7e1c06219c9
agioldas <aleksis.gioldaseas@outlook.com>
2026-01-02 02:42:23 +0200

    fixed and improved websocket handler logic

--- backend/services/live/internal/handlers/connect.go
+++ backend/services/live/internal/handlers/connect.go
@@ -1,5 +1,21 @@
 package handlers
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"social-network/shared/go/ct"
+	utils "social-network/shared/go/http-utils"
+	"social-network/shared/go/jwt"
+	tele "social-network/shared/go/telemetry"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
 // FOUND FROM DOCUMENTATION:
 
 // The message types are defined in RFC 6455, section 11.8.
@@ -24,161 +40,185 @@ package handlers
 // is UTF-8 encoded text.
 // PongMessage = 10
 
-// var upgrader = websocket.Upgrader{
-// 	CheckOrigin: func(r *http.Request) bool { return true },
-// }
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
 
-// func (h *Handlers) Connect() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		ctx := r.Context()
+func (h *Handlers) Connect() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("start websocket handler called")
 
-// 		fmt.Println("start websocket handler called")
+		ctx := r.Context()
+		claims, ok := utils.GetValue[jwt.Claims](r, ct.ClaimsKey)
+		if !ok {
+			tele.Error(ctx, "failed to fetch claims")
+			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "failed to fetch claims")
+		}
+		clientId := claims.UserId
 
-// 		websocketConn, cancelConn, err := upgradeConnection(w, r)
-// 		if err != nil {
-// 			tele.Error(ctx, "failed to upgrade connection to websocket")
-// 			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "something went wrong. Error: E314759")
-// 			return
-// 		}
-// 		defer cancelConn()
+		// UPGRADE
+		websocketConn, cancelConn, err := upgradeConnection(w, r)
+		if err != nil {
+			tele.Error(ctx, "failed to upgrade connection to websocket")
+			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "Something went wrong. Error: E314759")
+			return
+		}
+		defer cancelConn()
 
-// 		claims, ok := utils.GetValue[jwt.Claims](r, ct.ClaimsKey)
-// 		if !ok {
-// 			tele.Error(ctx, "failed to fetch claims")
-// 			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "failed to fetch claims")
-// 		}
+		connectionId, ok := ctx.Value(ct.ReqID).(string)
+		if !ok {
+			tele.Error(ctx, "failed get request id")
+			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "Something went wrong. Error: E741786")
+		}
 
-// 		clientId := claims.UserId
-// 		fmt.Println("Client id:", clientId)
+		channel := make(chan (string))
 
-// 		// context for cancelling send goroutine when connection closes, and for other goroutines to not send anything to channel that is no longer needed
-// 		ctx, cancelContext := context.WithCancel(context.Background())
+		var wg sync.WaitGroup
+		wg.Go(func() { websocketSender(ctx, connectionId, clientId, channel, websocketConn) })
+		websocketListener(websocketConn, clientId, connectionId)
+		wg.Wait()
 
-// 		connectionId, ok := ctx.Value(ct.ReqID).(string)
-// 		if !ok {
-// 			tele.Error(ctx, "failed to fetch claims")
-// 			utils.ErrorJSON(ctx, w, http.StatusInternalServerError, "Something went wrong. Error: E741786")
+		fmt.Println("ws handler closing")
+	}
+}
 
-// 		}
-// 		channel := make(chan (WSMessage))
+// routine that reads data coming from this client connection
+func websocketListener(websocketConn *websocket.Conn, clientId int64, connectionId string) {
+	for {
+		_, msg, err := websocketConn.ReadMessage()
+		if err != nil {
+			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+				break
+			}
+			log.Printf("clientId: %d, Start websocket error: unexpected read error: %v\n", clientId, err)
+			return
+		}
 
-// 		// wait group to wait for sender goroutine to stop
-// 		var wg sync.WaitGroup
+		messageString := string(msg)
+		if len(messageString) < 2 {
+			fmt.Println("invalid message received:", messageString)
+			return
+		}
 
-// 		wg.Add(1)
-// 		go websocketSender(connectionId, clientId, channel, websocketConn, ctx, &wg)
+		//do something with message received
+		//forward to chat service
+		//send to chat client
+	}
+}
 
-// 		websocketListener(websocketConn, clientId, connectionId)
+// Goroutine that sends data to this connection, it can pool messages if they arrive fast enough
+func websocketSender(ctx context.Context, connectionid string, clientId int64, channel <-chan string, conn *websocket.Conn) {
+	timer := time.NewTimer(time.Hour)
+	timer.Stop()
+	timerOn := false
+	lastFlushTimeStamp := time.Now().Add(-time.Hour)
+	poolingDuration := time.Millisecond * 500
 
-// 		// CLOSING PRECEDURE
+	messageBucket := []json.RawMessage{}
+	payloadBytes := []byte{}
+	var err error
+	for {
+		select {
+		case message := <-channel:
+			// message arrived, adding it to bucket
+			messageBucket = append(messageBucket, json.RawMessage(message))
 
-// 		// remove client so that no other goroutine attempts to send here
-// 		// clients.RemoveClientConnection(clientId, connectionId)
+			if timerOn {
+				// timer is already activated, therefore we just gather messages until the timer fires
+				continue
+			}
 
-// 		//adding message about online status here, may need to move
-// 		// sendUserStatusUpdate(clientId, "offline")
+			//check if more than 'poolingDuration' amount of time has passed after the last flush
+			if time.Since(lastFlushTimeStamp) <= poolingDuration {
+				//new message came too soon, so we'll just start the timer and wait it to ring before we flush
+				timer.Reset(poolingDuration - time.Since(lastFlushTimeStamp))
+				timerOn = true
+				continue
+			}
 
-// 		// remove all subscriptions associated with connectionId
-// 		// eventSystem.PurgeSubscriptions(connectionId)
+			// timer is off, and it's been a while since the last message, lets sending it immediately
 
-// 		// close the context to stop sender goroutine and other goroutines from sending
-// 		cancelContext()
+			// preparing message
+			payloadBytes, err = json.Marshal(messageBucket)
+			if err != nil {
+				fmt.Println("ERROR")
+				return
+			}
 
-// 		// wait for sender goroutine to stop
-// 		wg.Wait()
+			//sending message
+			err = conn.WriteMessage(websocket.TextMessage, payloadBytes)
+			if err != nil {
+				fmt.Printf("connectionid: %s, error on send message: clientid:%d err:%s \n", connectionid, clientId, err.Error())
+				return
+			}
 
-// 		fmt.Println("ws handler closing")
-// 	}
-// }
+			//clear the bucket
+			messageBucket = []json.RawMessage{}
+			lastFlushTimeStamp = time.Now()
 
-// func sendErrorToWS(websocketConn *websocket.Conn, payload string) {
-// 	errorMessage := []any{WSMessage{
-// 		Payload: payload,
-// 	}}
+			// activating timer so that if another message comes too soon,
+			// we gather it into the bucket instead of sending it immediately
+			timer.Reset(poolingDuration)
+			timerOn = true
 
-// 	bundledMessage, err := json.Marshal(errorMessage)
-// 	if err != nil {
-// 		fmt.Println("this isn't supposed to happen")
-// 		panic(1)
-// 	}
+		case <-timer.C:
+			timerOn = false
+			if len(messageBucket) == 0 {
+				// bucket is empty, therefore no need to send anything
+				continue
+			}
 
-// 	err = websocketConn.WriteMessage(websocket.TextMessage, bundledMessage)
-// 	if err != nil {
-// 		fmt.Println("failed to inform user that they have too many tabs open")
-// 	}
-// }
+			//preparing message
+			payloadBytes, err = json.Marshal(messageBucket)
+			if err != nil {
+				fmt.Println("ERROR")
+				return
+			}
 
-// // routine that reads data coming from this client connection
-// func websocketListener(websocketConn *websocket.Conn, clientId int64, connectionId int64) {
-// 	for {
-// 		_, msg, err := websocketConn.ReadMessage()
-// 		if err != nil {
-// 			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
-// 				break
-// 			}
-// 			log.Printf("clientId: %d, Start websocket error: unexpected read error: %v\n", clientId, err)
-// 			return
-// 		}
+			// bucket has piled up messages, so it's time to flush
+			err = conn.WriteMessage(websocket.TextMessage, payloadBytes)
+			if err != nil {
+				fmt.Printf("connectionid: %s, error on send message: clientid:%d err:%s \n", connectionid, clientId, err.Error())
+				return
+			}
 
-// 		messageString := string(msg)
-// 		if len(messageString) < 2 {
-// 			fmt.Println("invalid message received:", messageString)
-// 			return
-// 		}
+			//clearing bucket
+			messageBucket = []json.RawMessage{}
+			lastFlushTimeStamp = time.Now()
 
-// 	}
-// }
+		case <-ctx.Done():
+			timer.Stop()
+			return
+		}
+	}
+}
 
-// // Goroutine that sends data to this connection
-// func websocketSender(connectionid string, clientId int64, channel <-chan WSMessage, conn *websocket.Conn, ctx context.Context, wg *sync.WaitGroup) {
-// 	var timer *time.Timer
-// 	defer wg.Done()
-// 	for {
-// 		select {
-// 		case message := <-channel:
-// 			slice := []WSMessage{message}
-// 			bundledMessage, err := json.Marshal(slice)
-// 			if err != nil {
-// 				fmt.Println("AAAAA! A BAD JSON WAS GIVEN! The double encode failed! In production this situation will probably be ignored")
-// 				panic(1)
-// 				// TODO attempt individual sends? and drop w/e fails with a warning message
-// 			}
+func sendErrorToWS(websocketConn *websocket.Conn, payload string) {
+	errorMessage := []string{}
 
-// 			fmt.Println("sending", string(bundledMessage), "to", connectionid)
-// 			err = conn.WriteMessage(websocket.TextMessage, bundledMessage)
-// 			if err != nil {
-// 				fmt.Printf("connectionid: %d, error on send message: clientid:%d err:%s \n", connectionid, clientId, err.Error())
-// 				break outerLoop
-// 			}
-// 			break bundleLoop
+	bundledMessage, err := json.Marshal(errorMessage)
+	if err != nil {
+		fmt.Println("this isn't supposed to happen")
+		panic(1)
+	}
 
-// 		case <-ctx.Done():
-// 			if timer != nil {
-// 				if !timer.Stop() {
-// 					<-timer.C
-// 				}
-// 			}
-// 			break outerLoop
-// 		}
-// 	}
-// 	// TODO look into errors that can only happen when writing, this should cancel the entire connection
-// }
+	err = websocketConn.WriteMessage(websocket.TextMessage, bundledMessage)
+	if err != nil {
+		fmt.Println("failed to inform user that they have too many tabs open")
+	}
+}
 
-// func upgradeConnection(w http.ResponseWriter, r *http.Request) (*websocket.Conn, func(), error) {
-// 	websocketConn, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		utils.ErrorJSON(r.Context(), w, http.StatusInternalServerError, "failed websocket upgrade")
-// 		return nil, func() {}, fmt.Errorf("failed to upgrade connection to websocket: %w", err)
-// 	}
-// 	deferMe := func() {
-// 		err := websocketConn.Close()
-// 		if err != nil {
-// 			fmt.Println("failed to close websocket connection")
-// 		}
-// 	}
-// 	return websocketConn, deferMe, nil
-// }
-
-// type WSMessage struct {
-// 	Payload any `json:"message"`
-// }
+func upgradeConnection(w http.ResponseWriter, r *http.Request) (*websocket.Conn, func(), error) {
+	websocketConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		utils.ErrorJSON(r.Context(), w, http.StatusInternalServerError, "failed websocket upgrade")
+		return nil, func() {}, fmt.Errorf("failed to upgrade connection to websocket: %w", err)
+	}
+	deferMe := func() {
+		err := websocketConn.Close()
+		if err != nil {
+			fmt.Println("failed to close websocket connection")
+		}
+	}
+	return websocketConn, deferMe, nil
+}
