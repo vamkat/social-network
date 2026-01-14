d076e9895f2e6fe1c807f25cb15b55c60a7c18f8
Vagelis Stefanopoulos <vstefanopoulos@yahoo.gr>
2026-01-14 18:12:51 +0200

    option to retrive user on get messages

--- backend/services/chat/internal/application/private_conversations.app.go
+++ backend/services/chat/internal/application/private_conversations.app.go
@@ -92,10 +92,18 @@ func (c *ChatService) GetPreviousPMs(ctx context.Context,
 	if err := ct.ValidateStruct(arg); err != nil {
 		return res, ce.New(ce.ErrInvalidArgument, err, input)
 	}
+
 	res, err := c.Queries.GetPrevPrivateMsgs(ctx, arg)
 	if err != nil {
 		return res, ce.Wrap(nil, err, input)
 	}
+
+	if arg.RetrieveUsers {
+		if err := c.retrievePrivatMessageSenders(ctx, res.Messages, input); err != nil {
+			tele.Error(ctx, "failed to retrieve users for messages", "input", input, "error", err)
+		}
+	}
+
 	return res, nil
 }
 
@@ -111,6 +119,12 @@ func (c *ChatService) GetNextPMs(ctx context.Context,
 	if err != nil {
 		return res, ce.Wrap(nil, err, input)
 	}
+
+	if arg.RetrieveUsers {
+		if err := c.retrievePrivatMessageSenders(ctx, res.Messages, input); err != nil {
+			tele.Error(ctx, "failed to retrieve users for messages", "input", input, "error", err)
+		}
+	}
 	return res, nil
 }
 
@@ -125,6 +139,26 @@ func (c *ChatService) UpdateLastReadPrivateMsg(ctx context.Context, arg md.Updat
 	if err != nil {
 		tele.Error(ctx, "failed to publish private message to nats: @1", "error", err.Error())
 		return ce.Wrap(nil, err, input)
+	}
+	return nil
+}
+
+func (c *ChatService) retrievePrivatMessageSenders(ctx context.Context, msgs []md.PrivateMsg, input string) error {
+	allMemberIDs := make(ct.Ids, 0)
+	for _, r := range msgs {
+		allMemberIDs = append(allMemberIDs, r.Sender.UserId)
+	}
+
+	usersMap, err := c.RetriveUsers.GetUsers(ctx, allMemberIDs)
+	if err != nil {
+		return ce.Wrap(nil, err, input)
+	}
+
+	for i := range msgs {
+		retrieved := usersMap[msgs[i].Sender.UserId]
+		msgs[i].Sender.Username = retrieved.Username
+		msgs[i].Sender.AvatarId = retrieved.AvatarId
+		msgs[i].Sender.AvatarURL = retrieved.AvatarURL
 	}
 	return nil
 }
